9cf4e5daba8a8bc19896776179ebadbd1c186d64
thiagolboliveira1 <thiagolboliveira@hotmail.com>
2024-07-18 15:26:47 -0300

    Aprendendo mais sobre typescript

---
+++ C2 - Typescript para Angular/src/functions.ts
@@ -0,0 +1,22 @@
+
+//funções
+function addNumber(x: number, y: number): number {
+    return x + y;
+  }
+  
+  function addToHello(name: string): string {
+    return `Hello ${name}`;
+  }
+  
+  function CallToPhone(phone: number | string): number | string {
+    return phone;
+  }
+  
+  async function getDatabase(id: number): Promise<string> {
+    return "thiago";
+  }
+  
+  let soma: number = addNumber(4, 7);
+  
+  console.log();
+  
--- C2 - Typescript para Angular/src/index.ts
+++ C2 - Typescript para Angular/src/index.ts
@@ -1,19 +1,2 @@
-/**
- * variaveis
- */
-
-//tipos primitivos: boolean, number, string
-let ligado:boolean = false
-let nome: string = "Adriele";
-let idade:number = 28;
-let altura: number = 1.8;
-
-//tipos especiais null, undefined
-//null
-//undefined
-let nulo : null = null;
-let indefinido : undefined = undefined;
-
-//tipos abrangentes: any, void
-let returno:void
-let retornoView:any = false
+let dado: string = "Alice Santos Oliveira";
+console.log(dado);
